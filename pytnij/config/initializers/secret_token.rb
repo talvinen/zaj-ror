7b1d444045fb04fc3fbb435130c9b684ab2bc592
talvinen <drakul55@gmail.com>
2013-05-21 21:08:12 +0200

    rails new pytnij --- stworzenie projektu pytnij

---
+++ pytnij/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pytnij::Application.config.secret_token = '6d05725ab056eb2f130434928545078e0e52f1f17b3628a5c397a62934f8a4ca8fe6530f0f0e366c9b10dadd7fc4ccf72ad7cbe26cebe902e7c7e30c28912c07'
