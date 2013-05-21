f88bc8f856f4ce9d3543b1126f1470c94d8c65b5
talvinen <drakul55@gmail.com>
2013-05-21 21:43:51 +0200

    rake db:migrate -- po dodaniu pola email do post

--- pytnij/db/schema.rb
+++ pytnij/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130521193037) do
+ActiveRecord::Schema.define(:version => 20130521194032) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130521193037) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
   create_table "users", :force => true do |t|
