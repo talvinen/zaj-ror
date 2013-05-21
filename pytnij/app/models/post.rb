9343c11bcc7873d34fa0b42e4cae69ac7a1d3918
talvinen <drakul55@gmail.com>
2013-05-21 22:09:24 +0200

    modyfikacja modeli: comment -- walidacja ciala komentarza -- nie wysylaj pustego komentarza. post -- walidacja ciala i tytulu wiadomosci -- wiadomosc musi miec tytul i zawartosc oraz tytul musi byc unikatowy + asocjacja jeden do wielu do komentarzy oraz uzaleznienie komentarzy od funkcji destroy -- usun wiadomosc i wszytskie jej komentarze

--- pytnij/app/models/post.rb
+++ pytnij/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
+  validates_presence_of :body, :title
+  validates_uniqueness_of :title
+  has_many :comments, :dependent => :destroy
 end
